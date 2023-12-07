a35cf03f613bc8ccaedcac8e8d1ee2fc8f663c8c
jose_martinez4 <martinezcaceresjosem@gmail.com>
2023-12-07 00:12:22 -0400

    hotfix/README.md: instruccitions project, eliminated context not use

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import RouterView from "@/router";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { UserProvider } from "@/context/userContext";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { AuthLayout } from "./layouts/Auth/auth.style";
@@ -11,12 +10,10 @@ export const App: React.FC = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <UserProvider>
-          <AuthLayout>
-            <ToastContainer />
-            <RouterView />
-          </AuthLayout>
-        </UserProvider>
+        <AuthLayout>
+          <ToastContainer />
+          <RouterView />
+        </AuthLayout>
       </Provider>
     </BrowserRouter>
   );
